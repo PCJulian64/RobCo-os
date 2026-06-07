9ecfb111621e9db60756d3d32c256e06bc5827e3
PCJulian64 <parrajuliancoss@gmail.com>
2026-06-07 01:09:13 +0000

    Create boot.py

---
+++ robco-os/robco-shell/boot.py
@@ -0,0 +1,26 @@
+import time
+import os
+
+def boot():
+    os.system("clear")
+
+    print("ROBCO INDUSTRIES (TM)")
+    time.sleep(0.5)
+
+    print("\nINITIALIZING SYSTEM...")
+    time.sleep(1)
+
+    print("MEMORY CHECK ........ OK")
+    time.sleep(0.5)
+
+    print("NETWORK ............. OK")
+    time.sleep(0.5)
+
+    print("GRAPHICS ............ OK")
+    time.sleep(0.5)
+
+    print("AUDIO ............... OK")
+    time.sleep(0.5)
+
+    print("\nWELCOME USER")
+    time.sleep(1)
